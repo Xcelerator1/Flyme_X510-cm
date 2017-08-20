f933121f70c5ffb1bea59fb4f3ff65b0d4b7f896
Xcelerator1 <darkxcelerator@gmail.com>
2017-08-20 09:04:00 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ chmod a+x ~/bin/repo
 yes | repo init -u https://github.com/Rendyindo/manifest -b marshmallow-6.0
 repo sync -c
 source build/envsetup.sh
-cX510
+cv3750
 flyme fullota
 sudo apt install wput
 export DATEA=$(date +"%d%m%y")
-wput out/f*.zip ftp://Xcelerator:2SUKY5hT0vMq@uploads.androidfilehost.com:21/Flyme6-X510-$DATEA.zip
+wput out/f*.zip ftp://Xcelerator:2SUKY5hT0vMq@uploads.androidfilehost.com:21/Flyme6-v3750-$DATEA.zip
